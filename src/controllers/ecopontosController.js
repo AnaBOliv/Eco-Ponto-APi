d23667756bccf823866e08e6b02206eea995cdda
AnaBOliv <anaboliveira2319@gmail.com>
2026-06-15 15:37:26 -0300

    Create ecopontosController.js

---
+++ src/controllers/ecopontosController.js
@@ -0,0 +1,11 @@
+exports.listar = (req, res) => {};
+
+exports.buscarPorId = (req, res) => {};
+
+exports.cadastrar = (req, res) => {};
+
+exports.confirmar = (req, res) => {};
+
+exports.denunciar = (req, res) => {};
+
+exports.revalidar = (req, res) => {};
