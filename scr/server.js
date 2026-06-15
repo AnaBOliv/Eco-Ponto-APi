3af2993d6d6b4bd22d6147f8aa7024f547c67523
AnaBOliv <anaboliveira2319@gmail.com>
2026-06-15 14:57:41 -0300

    Update server.js

--- scr/server.js
+++ scr/server.js
@@ -1,1 +1,140 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let ecopontos = [];
+
+app.get("/", (req, res) => {
+  res.json({
+    projeto: "EcoPonto API",
+    status: "Online"
+  });
+});
+
+app.get("/ecopontos", (req, res) => {
+  res.json(ecopontos);
+});
+
+app.post("/ecopontos", async (req, res) => {
+  try {
+
+    const {
+      nome,
+      cep,
+      materiais,
+      diasFuncionamento,
+      horarioAbertura,
+      horarioFechamento,
+      responsavel
+    } = req.body;
+
+    const resposta = await axios.get(
+      `https://viacep.com.br/ws/${cep}/json/`
+    );
+
+    const endereco = resposta.data;
+
+    const novoPonto = {
+      id: ecopontos.length + 1,
+
+      nome,
+      cep,
+
+      logradouro: endereco.logradouro,
+      bairro: endereco.bairro,
+      cidade: endereco.localidade,
+      estado: endereco.uf,
+
+      materiais,
+
+      diasFuncionamento,
+
+      horarioAbertura,
+      horarioFechamento,
+
+      responsavel,
+
+      dataCadastro: new Date(),
+
+      ultimaValidacao: new Date(),
+
+      confirmacoes: 0,
+
+      denuncias: 0,
+
+      nivelConfianca: "Baixa",
+
+      status: "Ativo"
+    };
+
+    ecopontos.push(novoPonto);
+
+    res.status(201).json(novoPonto);
+
+  } catch (erro) {
+
+    res.status(500).json({
+      erro: "Erro ao consultar ViaCEP"
+    });
+
+  }
+});
+
+app.post("/ecopontos/:id/confirmar", (req, res) => {
+
+  const ponto = ecopontos.find(
+    p => p.id == req.params.id
+  );
+
+  if (!ponto) {
+    return res.status(404).json({
+      erro: "Ponto não encontrado"
+    });
+  }
+
+  ponto.confirmacoes++;
+
+  if (ponto.confirmacoes >= 16) {
+    ponto.nivelConfianca = "Alta";
+  }
+  else if (ponto.confirmacoes >= 6) {
+    ponto.nivelConfianca = "Media";
+  }
+
+  res.json(ponto);
+});
+
+app.post("/ecopontos/:id/denunciar", (req, res) => {
+
+  const ponto = ecopontos.find(
+    p => p.id == req.params.id
+  );
+
+  if (!ponto) {
+    return res.status(404).json({
+      erro: "Ponto não encontrado"
+    });
+  }
+
+  ponto.denuncias++;
+
+  if (ponto.denuncias >= 10) {
+    ponto.status = "Suspenso";
+  }
+  else if (ponto.denuncias >= 5) {
+    ponto.status = "Sob Analise";
+  }
+
+  res.json(ponto);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
